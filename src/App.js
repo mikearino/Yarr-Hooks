9107d94a93fb4139aef049ecadc3f69db722216a
Mike Lofgren <mikeylofgren@gmail.com>
2020-10-17 22:08:04 -0700

    add useState hook.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const App = () => {
+
+  // const [state, useState] = useState(initialState)
+  const [name, setName] = useState('')
+
   return (
     <div className="main-wrapper">
       <h1>Dishes</h1>
+      <input type="text" onChange={(e) => setName(e.target.value)} value={name}/>
+       <h3>{name}</h3>
     </div>
   );
 };
