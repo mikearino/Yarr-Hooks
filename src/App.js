81b03dcbc5c5d10669f0b1b489b188bc8651719d
Mike Lofgren <mikeylofgren@gmail.com>
2020-10-18 22:10:29 -0700

    add in useEffect to change document.title when React re-renders the page.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Refactor from './Toggle'
 
 const App = () => {
@@ -6,13 +6,16 @@ const App = () => {
   // const [state, useState] = useState(initialState)
   const [name, setName] = useState('')
 
+  useEffect(() => {
+    document.title = name;
+  }) 
   return (
     <div className="main-wrapper">
       <h1>Dishes</h1>
       <Refactor />
       <form onSubmit={(e) => {
         e.preventDefault();
-        formSubmit(name, setName)
+ 
       }
     }>
         <input type="text" onChange={(e) => setName(e.target.value)} value={name}/>
