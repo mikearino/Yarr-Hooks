5dfbcb41a9239062f872a3bb50234a0561886796
Mike Lofgren <mikeylofgren@gmail.com>
2020-10-18 22:30:47 -0700

    break out custom function into a reusable hook."

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Refactor from './Toggle'
+import { useTitleInput } from './hooks/useTitleInput'
 
 const App = () => {
+ // const [state, useState] = useState(initialState)
+  const [name, setName] = useTitleInput('')
 
-  // const [state, useState] = useState(initialState)
-  const [name, setName] = useState('')
-
-  useEffect(() => {
-    document.title = name;
-  }) 
   return (
     <div className="main-wrapper">
       <h1>Dishes</h1>
@@ -25,8 +22,4 @@ const App = () => {
   );
 };
 
-const formSubmit = (value, setValue) => {
-  console.log(`email sent to ${value}`)
-  setValue('')
-}
 export default App;
