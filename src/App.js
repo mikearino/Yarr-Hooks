7dadcb6bc7e1c5237450ba1ef4ede82e268ad66f
Mike Lofgren <mikeylofgren@gmail.com>
2020-10-21 22:49:17 -0700

    add in useRefs... the wrong way :).

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef } from 'react';
 import Refactor from './Toggle'
 import { useTitleInput } from './hooks/useTitleInput'
 
 const App = () => {
  // const [state, useState] = useState(initialState)
   const [name, setName] = useTitleInput('')
+  const ref = useRef();
+  console.log('ref:', ref)
 
   return (
-    <div className="main-wrapper">
-      <h1>Dishes</h1>
+    <div className="main-wrapper" ref={ref}>
+      <h1 onClick={() => ref.current.classList.add('new-fake-class')}>Dishes</h1>
       <Refactor />
       <form onSubmit={(e) => {
         e.preventDefault();
