10d3085fae2417d55e786dda1bc735dec0bd7a2f
Mike Lofgren <mikeylofgren@gmail.com>
2020-10-22 22:04:46 -0700

    add in context hook.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, createContext } from 'react';
 import Refactor from './Toggle'
 import { useTitleInput } from './hooks/useTitleInput'
+
+export const UserContext = createContext();
 
 const App = () => {
  // const [state, useState] = useState(initialState)
@@ -9,18 +11,23 @@ const App = () => {
   console.log('ref:', ref)
 
   return (
+    <UserContext.Provider
+    value={{
+      user: false
+    }}
+    >
     <div className="main-wrapper" ref={ref}>
       <h1 onClick={() => ref.current.classList.add('new-fake-class')}>Dishes</h1>
       <Refactor />
       <form onSubmit={(e) => {
-        e.preventDefault();
- 
+        e.preventDefault();        
       }
     }>
         <input type="text" onChange={(e) => setName(e.target.value)} value={name}/>
         <button>Submit</button>
       </form>
     </div>
+    </UserContext.Provider>
   );
 };
 
