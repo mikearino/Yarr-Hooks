0f3d6c1c7a5255f441d7eecee099f1d17bda912e
Mike Lofgren <mikeylofgren@gmail.com>
2020-10-27 22:17:35 -0700

    add in useReducer.

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,12 @@
 import React, {useReducer} from 'react';
 
+const initialState = { count: 0}
+
 function reducer(state, action) {
   switch (action.type) {
     case 'add':
       return {
-        count: state.count + 1
+        count: state.count + action.value
       }
       case 'minus':
       return {
@@ -12,7 +14,7 @@ function reducer(state, action) {
       }
       case 'reset':
       return {
-        count: 0
+        count: initialState.count
       }
       default:
         throw new Error()
@@ -23,9 +25,10 @@ const Counter = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
   return (
   <div>
-    <button>+</button>
-    <button>-</button>
-    <button>Reset</button>
+    <h3>{state.count}</h3>
+    <button onClick={() => dispatch({type: 'add', value: 10})}>+</button>
+    <button onClick={() => dispatch({type:'minus'})}>-</button>
+    <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
   </div>
   )
 }
